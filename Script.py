dd90a2218c7b0697fddadc7bcd7ff06001857068
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-01-13 17:49:55 -0500

    extracts parsing logic to separate functions

--- Script.py
+++ Script.py
@@ -6,7 +6,70 @@ import time
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
 x = np.arange(1)
+TEAM_PATH = 'team.txt'
+
+
+def fetch_raw_team() -> str:
+    with open(TEAM_PATH) as team_file:
+        raw_team = team_file.read()
+    return raw_team
+
+
+def parse_team(raw_team: str) -> list:
+    team = []
+    counter = 0
+    Status = ''
+
+    for line in raw_team.split('\n'):
+        line = line.strip()
+        if counter == 0:
+            lst = line.split(',')
+            lst2 = lst[0].split(' = ')
+            lst3 = lst[2].split(' = ')
+            lst4 = lst[3].split(' = ')
+            lst6 = lst[4].split(' = ')
+            lst7 = lst[5].split(' = ')
+            lst8 = lst[6].split(' = ')
+            name = int(lst2[1])
+            HP = int(lst3[1])
+            Max_HP = int(lst4[1])
+            Level = int(lst6[1])
+            if int(name) == 0:
+                Status = "Empty"
+                HP = "N"
+                Max_HP = "A"
+                Level = "N/A"
+            elif int(lst8[1]) == 1:
+                name = "egg"
+                Status = "Egg"
+                HP = "N"
+                Max_HP = "A"
+                Level = "N/A"
+            elif int(lst3[1]) == 0:
+                Status = "Fainted"
+            elif int(lst7[1]) == 0:
+                Status = "Healthy"
+            elif int(lst7[1]) in range(1, 7):
+                Status = "Sleep"
+            elif int(lst7[1]) == 8:
+                Status = "Poisoned"
+            elif int(lst7[1]) == 16:
+                Status = "Burned"
+            elif int(lst7[1]) == 32:
+                Status = "Frozen"
+            elif int(lst7[1]) == 64:
+                Status = "Paralyzed"
+            elif int(lst7[1]) == 128:
+                Status = "Toxic"
+            pokemon = [name, HP, Max_HP, Level, Status]
+            team.append(pokemon)
+        if counter == 2:
+            counter = -1
+        counter += 1
+    return team
+
 
 def make_plot(maxHP,current,output):
     plt.clf()
@@ -18,78 +81,20 @@ def make_plot(maxHP,current,output):
         plt.barh(x, current, color='#FFC107', height = 50/96) #yellow
     else:
         plt.barh(x, current, color='#F44336', height = 50/96) #red
-    plt.barh(x, maxHP-current,left = current, color='#212121', height = 50/96) #background        
+    plt.barh(x, maxHP-current,left = current, color='#212121', height = 50/96) #background
     plt.axis('off')
     plt.savefig(output,bbox_inches='tight', pad_inches = 0, transparent=True, dpi = 96)
 
 
 def main():
-    filename = 'team.txt'
-    team = []
-    pokemon = []
-    member = 0
-    counter = 0
-    name = 0
-    HP = 0
-    Max_HP = 0
-    lst = []
-    lst2 = []
-    lst3 = []
-    lst4 = []
+    saved_state = ''
+    fresh_state = fetch_raw_team()
     while True:
+        if not fresh_state or saved_state == fresh_state:
+            continue
+        saved_state = fresh_state
+        team = parse_team(saved_state)
         try:
-            team = []
-            for line in open(filename):
-                line = line.strip()
-                if counter ==0:
-                    lst = line.split(',')
-                    lst2 = lst[0].split(' = ')
-                    lst3 = lst[2].split(' = ')
-                    lst4 = lst[3].split(' = ')
-                    lst5 = lst[1].split(' = ')
-                    lst6 = lst[4].split(' = ')
-                    lst7 = lst[5].split(' = ')
-                    lst8 = lst[6].split(' = ')
-                    name = int(lst2[1])
-                    HP = int(lst3[1])
-                    Max_HP = int(lst4[1])
-                    Level = int(lst6[1])
-                    if int(name) == 0:
-                        Status = "Empty"
-                        HP = "N"
-                        Max_HP = "A"
-                        Level = "N/A"
-                    elif int(lst8[1]) == 1:
-                        name = "egg"
-                        Status = "Egg"
-                        HP = "N"
-                        Max_HP = "A"
-                        Level = "N/A"
-                    elif int(lst3[1]) == 0:
-                        Status = "Fainted"
-                    elif int(lst7[1]) == 0:
-                        Status = "Healthy"
-                    elif int(lst7[1]) in range(1,7):
-                        Status = "Sleep"
-                    elif int(lst7[1]) == 8:
-                        Status = "Poisoned"
-                    elif int(lst7[1]) == 16:
-                        Status = "Burned"
-                    elif int(lst7[1]) == 32:
-                        Status = "Frozen"
-                    elif int(lst7[1]) == 64:
-                        Status = "Paralyzed"
-                    elif int(lst7[1]) == 128:
-                        Status = "Toxic"
-                    pokemon = [name, HP, Max_HP, Level, Status]
-                    team.append(pokemon)
-                    # HP = 105
-                    # Max_HP = 420
-                    # my_dpi = 96 # find this out at https://www.infobyip.com/detectmonitordpi.php
-                    # y = HP/Max_HP
-                if counter == 2:
-                    counter = -1
-                counter +=1
             shutil.copyfile("C:\\Users\\Daniel\\Pictures\\Twitch\\sugimori\\" + str(team[0][0]) + ".png", "C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\team images\\__party1.png")
             shutil.copyfile("C:\\Users\\Daniel\\Pictures\\Twitch\\sugimori\\" + str(team[1][0]) + ".png", "C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\team images\\__party2.png")
             shutil.copyfile("C:\\Users\\Daniel\\Pictures\\Twitch\\sugimori\\" + str(team[2][0]) + ".png", "C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\team images\\__party3.png")
@@ -167,4 +172,5 @@ def main():
                 make_plot(team[5][2],team[5][1],'health6.png')        
         except Exception:
             pass
+        break
 main()
